477f1f8fc1d6b352db7dcde00a613aa906e4d6f1
João Marcos <jmarcosmmn@gmail.com>
2024-09-13 07:57:29 -0300

    feature: enviroment variables

--- mediumtutorial/settings.py
+++ mediumtutorial/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w0qt7#u%s-l&a5%sw9y5tfaap!tctw0n$x#snnf&d%pxh)jy6f'
+# Carregar variáveis do arquivo .env
+load_dotenv()
+# Acessar variáveis de ambiente
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
